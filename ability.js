f94837eb90fba14f898c6da39ee265e5ce5a6863
CallumABeale <callum.beale.098@gmail.com>
2021-11-01 14:15:26 +1000

    Ability class framework added

---
+++ ability.js
@@ -0,0 +1,21 @@
+class Ability {
+	constructor() {
+		this.sprite;
+		this.animations = {};
+		this.effect;
+		this.damage;
+	}
+	p5Load() {
+		// Call in preload
+		// load images for animation
+	}
+	p5Init() {
+		// Call in setup
+		// Create sprite && set sprite settings
+	}
+	update() {
+		// Call in draw
+		// Call controls && idle animation
+	}
+	idle() {}
+}
