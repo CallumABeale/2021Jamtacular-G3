465a86810c4e78895850ff20a774856e4dcf9bd9
CallumABeale <callum.beale.098@gmail.com>
2021-11-01 14:07:19 +1000

    PowerUp class framework created

---
+++ powerUp.js
@@ -0,0 +1,18 @@
+class PowerUp {
+	constructor() {
+		this.effect;
+	}
+	p5Load() {
+		// Call in preload
+		// load images for animation
+	}
+	p5Init() {
+		// Call in setup
+		// Create sprite && set sprite settings
+	}
+	update() {
+		// Call in draw
+		// Call controls && idle animation
+	}
+	idle() {}
+}
