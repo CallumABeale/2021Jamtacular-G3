025c89948f8fb31411eba82b660d06c7cd89aa41
CallumABeale <callum.beale.098@gmail.com>
2021-11-08 16:30:21 +1000

    Abilities added to player

--- p5PlayStartpoint/player.js
+++ p5PlayStartpoint/player.js
@@ -7,6 +7,9 @@ class Player {
 		this.abillityList = [];
 		this.activeItem;
 		this.itemList = [];
+		this.lightning = new Ability('lightning');
+		this.freeze = new Ability('freeze');
+		this.telekenises = new Ability('telekenises');
 	}
 	p5Load() {
 		// Call in preload
@@ -15,7 +18,12 @@ class Player {
 	p5Init(x = width * 0.2, y = height * 0.8) {
 		// Call in setup
 		// Create sprite && set sprite settings
-		this.sprite = createSprite((testSprite.position.x), (testSprite.position.y), CANVASWIDTH / resolution / 5, CANVASHEIGHT / resolution / 1.5);
+		this.sprite = createSprite(
+			testSprite.position.x,
+			testSprite.position.y,
+			CANVASWIDTH / resolution / 5,
+			CANVASHEIGHT / resolution / 1.5
+		);
 		this.sprite.friction = 0.1;
 		// this.sprite.setCollider("circle", 0, 0, CANVASWIDTH/resolution/25)
 		this.sprite.debug = true;
@@ -27,33 +35,36 @@ class Player {
 		// Call controls && idle animation
 		if (this.sprite.collide(groundGroup)) {
 			gravity = 1;
-			if (this.sprite.touching.top){
-			this.sprite.position.y+=200/resolution
-			this.sprite.velocity.y = 1
+			if (this.sprite.touching.top) {
+				this.sprite.position.y += 200 / resolution;
+				this.sprite.velocity.y = 1;
 			} else {
-				if (this.sprite.touching.bottom){
+				if (this.sprite.touching.bottom) {
 					this.sprite.jumpActive = true;
 					this.sprite.velocity.y = 0;
 				}
 			}
 		}
 
-		if (this.sprite.collide(antiGrav) && keyIsDown(32) && this.sprite.touching.bottom) {
+		if (
+			this.sprite.collide(antiGrav) &&
+			keyIsDown(32) &&
+			this.sprite.touching.bottom
+		) {
 			this.sprite.jumpActive = false;
 			gravity = -1;
 		}
 
-
 		if (this.sprite.collide(walls)) {
-			if (gravity == -1){
+			if (gravity == -1) {
 				gravity = 1;
 			}
-			if ( this.sprite.touching.top){
-			this.sprite.position.y+=50/resolution
-			this.sprite.velocity.y = 1
+			if (this.sprite.touching.top) {
+				this.sprite.position.y += 50 / resolution;
+				this.sprite.velocity.y = 1;
 			}
-			if (this.sprite.touching.bottom){
-				this.sprite.velocity.y =0;
+			if (this.sprite.touching.bottom) {
+				this.sprite.velocity.y = 0;
 				this.sprite.jumpActive = true;
 			}
 		}
@@ -65,7 +76,7 @@ class Player {
 	idle() {
 		// this.sprite.changeAnimation("standing")
 	}
-	castAbility() { }
+	castAbility() {}
 	cycleAbility() {
 		if (abilityList.includes(activeAbility)) {
 			let index = abilityList.indexOf(activeAbility);
@@ -76,14 +87,14 @@ class Player {
 			}
 		}
 	}
-	useItem() { }
-	cycleItem() { }
+	useItem() {}
+	cycleItem() {}
 	controls() {
 		/**
 		 * always applying gravity
 		 */
 		if (!this.sprite.collide(groundGroup)) {
-			this.sprite.velocity.y += gravity
+			this.sprite.velocity.y += gravity;
 		}
 
 		/**
@@ -95,7 +106,7 @@ class Player {
 				this.sprite.velocity.x -= 0.5;
 			}
 			this.sprite.mirrorX(Math.sign(this.sprite.velocity.x));
-			this.sprite.velocity.x -= .5;
+			this.sprite.velocity.x -= 0.5;
 		}
 		/**
 		 * If RIGHT_ARROW is pressed, move right
@@ -107,12 +118,16 @@ class Player {
 			}
 			// this.sprite.changeAnimation("moving")
 			this.sprite.mirrorX(Math.sign(this.sprite.velocity.x));
-			this.sprite.velocity.x += .5;
+			this.sprite.velocity.x += 0.5;
 		}
 		/**
 		 * If UP_ARROW is pressed, jump
 		 */
-		if (keyIsDown(32) && !this.sprite.collide(antiGrav) && !this.sprite.touching.bottom) {
+		if (
+			keyIsDown(32) &&
+			!this.sprite.collide(antiGrav) &&
+			!this.sprite.touching.bottom
+		) {
 			if (this.sprite.jumpActive == true && this.sprite.velocity.y < 2) {
 				this.sprite.velocity.y = -groundGroup[0].height / 1.25;
 				this.sprite.jumpActive = false;
@@ -133,7 +148,9 @@ class Player {
 		/**
 		 * If R is pressed, cycle ability
 		 */
-		if (keyIsDown(82)) [this.cycleAbility()];
+		if (keyIsDown(82)) {
+			this.cycleAbility();
+		}
 		if (keyIsDown(70)) {
 			this.cycleItem();
 		}
