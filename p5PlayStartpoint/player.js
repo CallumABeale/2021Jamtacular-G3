43e67644192edd100f654047146a1712ca67f231
CallumABeale <callum.beale.098@gmail.com>
2021-11-11 11:52:55 +1000

    Player class merge conflict solved

--- p5PlayStartpoint/player.js
+++ p5PlayStartpoint/player.js
@@ -37,17 +37,12 @@ class Player {
 	update() {
 		// Call in draw
 		// Call controls && idle animation
-<<<<<<< Updated upstream
-		if (keyIsDown(90) && camera.zoom > (zoomLevel / resolution) * 3) {
-			camera.zoom *= 0.95;
-=======
 		if (this.abilityCooldown > 0) {
 			this.abilityCooldown--;
 			console.log(this.sprite.abilityCooldown);
 		}
-		if (keyIsDown(90) && camera.zoom > zoomLevel / resolution * 3) {
-			camera.zoom *= 0.95
->>>>>>> Stashed changes
+		if (keyIsDown(90) && camera.zoom > (zoomLevel / resolution) * 3) {
+			camera.zoom *= 0.95;
 		}
 		if (this.sprite.collide(groundGroup)) {
 			gravity = 1;
@@ -60,91 +55,88 @@ class Player {
 					this.sprite.velocity.y = 0;
 				}
 			}
-<<<<<<< Updated upstream
+		}
+		if (
+			this.sprite.collide(bouncingBridges) &&
+			!this.sprite.collide(activeGrav)
+		) {
+			gravity = 1;
+			if (this.sprite.touching.top) {
+				this.sprite.position.y += 200 / resolution;
+				this.sprite.velocity.y = 1;
+			} else {
+				if (this.sprite.touching.bottom) {
+					this.sprite.jumpActive = true;
+					this.sprite.velocity.y = 0;
+				}
+			}
 		}
 
 		if (this.sprite.collide(walls)) {
 			if (gravity == -1) {
-=======
-			}
-			if (this.sprite.collide(bouncingBridges) && !this.sprite.collide(activeGrav)) {
->>>>>>> Stashed changes
 				gravity = 1;
-				if (this.sprite.touching.top) {
-					this.sprite.position.y += 200 / resolution;
-					this.sprite.velocity.y = 1;
-				}
-				else {
-					if (this.sprite.touching.bottom) {
-						this.sprite.jumpActive = true;
-						this.sprite.velocity.y = 0;
-					}
-				}
-				}
+			}
+			if (this.sprite.touching.top) {
+				this.sprite.position.y += 50 / resolution;
+				this.sprite.velocity.y = 1;
+			}
+			if (this.sprite.touching.bottom) {
+				this.sprite.velocity.y = 0;
+				this.sprite.jumpActive = true;
+			}
+		}
+		this.controls();
+		if (int(this.sprite.velocity.x) === 0) {
+			this.idle();
+		}
+	}
+	idle() {
+		// this.sprite.changeAnimation("standing")
+	}
+	castAbility() {
+		this.activeAbility.cast(this.sprite.position.x, this.sprite.position.y);
+	}
+	cycleAbility() {
+		if (this.abilityList.includes(this.activeAbility)) {
+			let index = this.abilityList.indexOf(this.activeAbility);
+			if (index === this.abilityList.length - 1) {
+				this.activeAbility = this.abilityList[0];
+			} else {
+				this.activeAbility = this.abilityList[index + 1];
+			}
+		}
+	}
+	useItem() {}
+	cycleItem() {}
+	controls() {
+		/**
+		 * always applying gravity
+		 */
+		if (this.sprite.collide(bouncingBridges)) {
+			this.sprite.jumpActive = true;
+			this.sprite.velocity.y = 0;
+			this.sprite.friction = 0;
+			console.log('jumpActive and friction');
+			if (keyIsDown(LEFT_ARROW)) {
+				this.sprite.velocity.x = bouncingBridges[0].velocity.x;
+				this.sprite.mirrorX(Math.sign(this.sprite.velocity.x));
+				this.sprite.velocity.x -= 0.5;
+			} else if (keyIsDown(RIGHT_ARROW)) {
+				this.sprite.velocity.x = bouncingBridges[0].velocity.x;
+				this.sprite.mirrorX(Math.sign(this.sprite.velocity.x));
+				this.sprite.velocity.x += 0.5;
+			} else {
+				this.sprite.velocity.x = bouncingBridges[0].velocity.x;
+			}
+		}
+		if (!this.sprite.collide(bouncingBridges)) {
+			this.sprite.friction = 0.1;
+		}
 
+		if (!this.sprite.collide(groundGroup)) {
+			this.sprite.velocity.y += gravity;
+		}
 
-				if (this.sprite.collide(walls)) {
-					if (gravity == -1) {
-						gravity = 1;
-					}
-					if (this.sprite.touching.top) {
-						this.sprite.position.y += 50 / resolution;
-						this.sprite.velocity.y = 1;
-					}
-					if (this.sprite.touching.bottom) {
-						this.sprite.velocity.y = 0;
-						this.sprite.jumpActive = true;
-					}
-				}
-				this.controls();
-				if (int(this.sprite.velocity.x) === 0) {
-					this.idle();
-				}
-			}
-			idle() {
-				// this.sprite.changeAnimation("standing")
-			}
-			castAbility() {
-				this.activeAbility.cast(this.sprite.position.x, this.sprite.position.y);
-			}
-			cycleAbility() {
-				if (this.abilityList.includes(this.activeAbility)) {
-					let index = this.abilityList.indexOf(this.activeAbility);
-					if (index === this.abilityList.length - 1) {
-						this.activeAbility = this.abilityList[0];
-					} else {
-						this.activeAbility = this.abilityList[index + 1];
-					}
-				}
-			}
-			useItem() { }
-			cycleItem() { }
-			controls() {
-				/**
-				 * always applying gravity
-				 */
-				if (this.sprite.collide(bouncingBridges)) {
-					this.sprite.jumpActive = true;
-					this.sprite.velocity.y = 0;
-					this.sprite.friction = 0;
-					console.log('jumpActive and friction')
-					if (keyIsDown(LEFT_ARROW)) {
-						this.sprite.velocity.x = bouncingBridges[0].velocity.x;
-						this.sprite.mirrorX(Math.sign(this.sprite.velocity.x));
-						this.sprite.velocity.x -= 0.5;
-					} else if (keyIsDown(RIGHT_ARROW)) {
-						this.sprite.velocity.x = bouncingBridges[0].velocity.x;
-						this.sprite.mirrorX(Math.sign(this.sprite.velocity.x));
-						this.sprite.velocity.x += 0.5;
-					} else {
-						this.sprite.velocity.x = bouncingBridges[0].velocity.x;
-					}
-				}
-				if (!this.sprite.collide(bouncingBridges)) {
-					this.sprite.friction = 0.1
-				}
-
-<<<<<<< Updated upstream
 		/**
 		 * If LEFT_ARROW is pressed, move left
 		 */
@@ -166,101 +158,52 @@ class Player {
 		 * If UP_ARROW is pressed on a antigrav pad, reverse gravity,
 		 * else jump
 		 */
-		if (
-			this.sprite.collide(antiGrav) &&
-			keyIsDown(32) &&
-			this.sprite.touching.bottom
-		) {
+		if (this.sprite.collide(activeGrav) && keyIsDown(32)) {
+			this.sprite.jumpActive = false;
+			this.sprite.position.y -= resolution * 2;
 			this.sprite.jumpActive = false;
 			gravity = -1;
 		} else {
 			if (
 				keyIsDown(32) &&
-				!this.sprite.collide(antiGrav) &&
-				!this.sprite.touching.bottom
+				this.sprite.collide(antiGrav) &&
+				this.sprite.touching.bottom
 			) {
-				if (this.sprite.jumpActive == true && this.sprite.velocity.y < 2) {
-					this.sprite.velocity.y = -groundGroup[0].height / 1.25;
-					this.sprite.jumpActive = false;
-					this.sprite.limitSpeed = groundGroup[0].height / 1.25;
-=======
-				if (!this.sprite.collide(groundGroup)) {
-					this.sprite.velocity.y += gravity;
-				}
-
-				/**
-				 * If LEFT_ARROW is pressed, move left
-				 */
-				if (keyIsDown(LEFT_ARROW)) {
-					// this.sprite.changeAnimation("moving")
-					this.sprite.mirrorX(Math.sign(this.sprite.velocity.x));
-					this.sprite.velocity.x -= 0.5;
-				}
-				/**
-				 * If RIGHT_ARROW is pressed, move right
-				 * If SHIFT is pressed, it will move twice as fast
-				 */
-				if (keyIsDown(RIGHT_ARROW)) {
-					// this.sprite.changeAnimation("moving")
-					this.sprite.mirrorX(Math.sign(this.sprite.velocity.x));
-					this.sprite.velocity.x += 0.5;
-				}
-				/**
-				 * If UP_ARROW is pressed on a antigrav pad, reverse gravity, 
-				 * else jump
-				 */
-				if (this.sprite.collide(activeGrav) &&
-					keyIsDown(32)
-				) {
-					this.sprite.jumpActive = false;
-					this.sprite.position.y-=resolution*2
-					this.sprite.jumpActive = false;
-					gravity = -1;
-				} else {
-					if (
-						keyIsDown(32) &&
-						this.sprite.collide(antiGrav) &&
-						this.sprite.touching.bottom
-					) {
+				this.sprite.jumpActive = false;
+				this.sprite.position.y -= resolution * 2;
+				gravity = -1;
+			} else {
+				if (keyIsDown(32) && !this.sprite.collide(antiGrav)) {
+					if (this.sprite.jumpActive == true && this.sprite.velocity.y < 2) {
+						this.sprite.position.y -= resolution * 2;
+						this.sprite.velocity.y = -walls[0].height / 6;
 						this.sprite.jumpActive = false;
-						this.sprite.position.y-=resolution*2
-						gravity = -1;
-					} else {
-						if (
-							keyIsDown(32) &&
-							!this.sprite.collide(antiGrav)
-						) {
-							if (this.sprite.jumpActive == true && this.sprite.velocity.y < 2) {
-								this.sprite.position.y -= resolution * 2;
-								this.sprite.velocity.y = -walls[0].height / 6;
-								this.sprite.jumpActive = false;
-								this.sprite.limitSpeed = walls[0].height / 5;
-							}
-						}
+						this.sprite.limitSpeed = walls[0].height / 5;
 					}
-				}
-				/**
-				 * If Q is pressed, use ability
-				 */
-				if (this.abilityCooldown == 0 && keyWentDown(81)) {
-					this.castAbility();
-					this.abilityCooldown = 30
-				}
-				/**
-				 * If E is pressed, use item
-				 */
-				if (keyWentDown(69)) {
-					this.useItem();
-				}
-				/**
-				 * If R is pressed, cycle ability
-				 */
-				if (keyWentDown(82)) {
-					this.cycleAbility();
-				}
-				if (keyWentDown(70)) {
-					this.cycleItem();
->>>>>>> Stashed changes
 				}
 			}
 		}
+		/**
+		 * If Q is pressed, use ability
+		 */
+		if (this.abilityCooldown == 0 && keyWentDown(81)) {
+			this.castAbility();
+			this.abilityCooldown = 30;
+		}
+		/**
+		 * If E is pressed, use item
+		 */
+		if (keyWentDown(69)) {
+			this.useItem();
+		}
+		/**
+		 * If R is pressed, cycle ability
+		 */
+		if (keyWentDown(82)) {
+			this.cycleAbility();
+		}
+		if (keyWentDown(70)) {
+			this.cycleItem();
+		}
+	}
+}
